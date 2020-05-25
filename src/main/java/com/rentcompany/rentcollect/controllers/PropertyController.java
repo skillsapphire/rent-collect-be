68f839d71aad380083bd299cc5a0b9fc227c460b
MP <mp@gmail.com>
2020-05-25 22:54:09 +0530

    pagination

--- src/main/java/com/rentcompany/rentcollect/controllers/PropertyController.java
+++ src/main/java/com/rentcompany/rentcollect/controllers/PropertyController.java
@@ -3,7 +3,9 @@ package com.rentcompany.rentcollect.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rentcompany.rentcollect.exception.ResourceNotFoundException;
@@ -34,9 +37,16 @@ public class PropertyController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Value("${spring.data.web.pageable.default-page-size}")
+	private int pageSize;
+	
 	@GetMapping("/property/{userId}")
 	@PreAuthorize("hasRole('ADMIN')")
-	public Page<Property> getAllProprties(@PathVariable(value = "userId") Long userId, Pageable pageable) {
+	public Page<Property> getAllProprties(@PathVariable(value = "userId") Long userId, 
+			@RequestParam("page") int page) {
+		//https://dzone.com/articles/pagination-in-springboot-applications
+		//http://zetcode.com/springboot/pagination/
+		Pageable pageable = PageRequest.of(page, pageSize);
 		return propertyRepository.findByUserId(userId, pageable);
 	}
 	
--- src/main/java/com/rentcompany/rentcollect/repository/PropertyRepository.java
+++ src/main/java/com/rentcompany/rentcollect/repository/PropertyRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rentcompany.rentcollect.models.Property;
@@ -14,6 +15,8 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
 	//https://docs.spring.io/spring-data/data-jpa/docs/current/reference/html/#repositories.query-methods.query-creation
 	//https://www.petrikainulainen.net/programming/spring-framework/spring-data-jpa-tutorial-three-custom-queries-with-query-methods/
 	//https://www.javaworld.com/article/3387643/java-persistence-with-jpa-and-hibernate-part-2-many-to-many-relationships.html
+
 	Page<Property> findByUserId(Long userId, Pageable pageable);
+	
 	Optional<Property> findByIdAndUserId(Long id, Long userId);
 }
